94bbdad10410225ec6b7f2ec772088df165ceb72
Andrew Sun <andrew@crossbowffs.com>
2020-08-09 15:24:43 -0700

    Fix cargo path

--- server/run-server.sh
+++ server/run-server.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 script_dir="$(dirname "$0")"
 . "${script_dir}/../env"
-cargo run --release -- --latitude=${LATITUDE} --longitude=${LONGITUDE} --data="${script_dir}/../data/current.csv"
+cargo run --manifest-path="${script_dir}/Cargo.toml" --release -- --latitude=${LATITUDE} --longitude=${LONGITUDE} --data="${script_dir}/../data/current.csv"
