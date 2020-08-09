8edc7b425bfa1340847f16ddc0bf33761882f1a2
Andrew Sun <andrew@crossbowffs.com>
2020-08-09 15:28:41 -0700

    Workaround for template path

--- server/run-server.sh
+++ server/run-server.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 script_dir="$(dirname "$0")"
 . "${script_dir}/../env"
+cd "${script_dir}"
 cargo run --manifest-path="${script_dir}/Cargo.toml" --release -- --latitude=${LATITUDE} --longitude=${LONGITUDE} --data="${script_dir}/../data/current.csv"
