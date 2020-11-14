81e69623847ad3b8f6c191f3a48ce9400cc50f66
Andrew Sun <andrew@crossbowffs.com>
2020-11-14 08:27:10 -0800

    Add iOS/fallback navigation URLs

--- server/static/gastrak.js
+++ server/static/gastrak.js
@@ -10,6 +10,16 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
+function getNavigationUrl(lat, long) {
+    if (/Android/.test(navigator.userAgent)) {
+        return `google.navigation:q=${lat},${long}`;
+    } else if (/iPhone|iPad|iPod/.test(navigator.userAgent)) {
+        return `http://maps.apple.com/?daddr=${lat},${long}&dirflg=d`;
+    } else {
+        return `https://www.google.com/maps/?q=@${lat},${long}`;
+    }
+}
+
 let lines = gastrakData.split('\n').filter((l) => l !== '');
 for (let line of lines) {
     let cols = line.split(',');
@@ -17,8 +27,9 @@ for (let line of lines) {
     let lat = parseFloat(cols[3]);
     let long = parseFloat(cols[4]);
     let price = cols[5];
+    let url = getNavigationUrl(lat, long);
     L.marker([lat, long]).bindTooltip(
-        `<a href="google.navigation:q=${lat},${long}"><b>${name}</b></a><br>\$${price}`, {
+        `<a href="${url}"><b>${name}</b></a><br>\$${price}`, {
             direction: "top",
             permanent: true,
             offset: L.point(0, -16),
