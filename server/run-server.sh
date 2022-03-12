401976676898b47ac9363abdc8cc950c5fd4dea2
Andrew Sun <andrew@crossbowffs.com>
2022-03-12 00:24:35 -0800

    Fix server script paths

--- server/run-server.sh
+++ server/run-server.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+set -euo pipefail
 script_dir="$(dirname "$0")"
 . "${script_dir}/../env"
+
 cd "${script_dir}"
-go run main.go \
+go run "main.go" \
     --latitude=${LATITUDE} \
     --longitude=${LONGITUDE} \
-    --data="${script_dir}/../data/current.csv" \
-    --history="${script_dir}/../data/history.csv" \
+    --data="../data/current.csv" \
+    --history="../data/history.csv" \
     --port="${SERVER_PORT}"
