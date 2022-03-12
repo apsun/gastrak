3a67ddcfd8851e578a0dd21251274477dd560b52
Andrew Sun <andrew@crossbowffs.com>
2022-03-11 23:29:50 -0800

    Don't clobber current data if fetch fails

--- data/run-gastrak.sh
+++ data/run-gastrak.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+set -euo pipefail
 script_dir="$(dirname "$0")"
 . "${script_dir}/../env"
-go run "${script_dir}/gastrak.go" -latitude=${LATITUDE} -longitude=${LONGITUDE} | tee "${script_dir}/current.csv" >> "${script_dir}/history.csv"
+
+go run "${script_dir}/gastrak.go" -latitude=${LATITUDE} -longitude=${LONGITUDE} > "${script_dir}/current.csv.new"
+cat "${script_dir}/current.csv.new" >> "${script_dir}/history.csv"
+mv "${script_dir}/current.csv.new" "${script_dir}/current.csv"
